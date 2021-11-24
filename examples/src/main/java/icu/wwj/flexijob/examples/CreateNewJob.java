582871c4f3f4cc84d6dedde81be48ab99a13008f
吴伟杰 <wuweijie@apache.org>
2021-11-25 00:13:08 +0800

    Update examples for new API

--- examples/src/main/java/icu/wwj/flexijob/examples/CreateNewJob.java
+++ examples/src/main/java/icu/wwj/flexijob/examples/CreateNewJob.java
@@ -18,15 +18,24 @@ package icu.wwj.flexijob.examples;
 
 import icu.wwj.flexijob.FlexijobClient;
 import icu.wwj.flexijob.examples.job.DemoJob;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.shardingsphere.elasticjob.api.JobConfiguration;
 import org.apache.shardingsphere.elasticjob.reg.base.CoordinatorRegistryCenter;
 
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
 public class CreateNewJob {
     
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         CoordinatorRegistryCenter registryCenter = RegistryCenters.createRegistryCenter("127.0.0.1:2181,127.0.0.1:2182,127.0.0.1:2183", "flexijob-examples");
         JobConfiguration jobConfiguration = JobConfiguration.newBuilder("flexijob-0", 3).cron("0/10 * * * * ?").build();
         FlexijobClient client = new FlexijobClient(registryCenter);
+        log.info("Adding job [{}]...", jobConfiguration.getJobName());
         client.addJob(jobConfiguration, DemoJob.class);
+        log.info("Shutdown job [{}] after 25 seconds.", jobConfiguration.getJobName());
+        TimeUnit.SECONDS.sleep(25);
+        log.info("Shutting down job [{}]...", jobConfiguration.getJobName());
+        client.removeJob(jobConfiguration.getJobName());
     }
 }
